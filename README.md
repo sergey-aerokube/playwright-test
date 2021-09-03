f953aee451f02c57defa3474de7aa39121ce1952
sergey <sergey@aerokube.com>
2021-09-03 08:51:57 +0300

    fix test.js

--- README.md
+++ README.md
@@ -4,11 +4,10 @@
 ```
 docker build -t plw:0.0.1 .
 docker run -it --name plw \
- -e CLIENT_URL={{ url }} \
- -e CLIENT_CREDENTIALS="{{ username }}:{{ password }}" \
+ -e URL={{ url }} \
+ -e CREDENTIALS="{{ username }}:{{ password }}" \
 plw:0.0.1
 
 docker cp plw:/usr/app/screenshot.png ./
 docker rm plw
 ```
-
--- test.js
+++ test.js
@@ -1,10 +1,16 @@
 const { chromium } = require('playwright');
 
 var url         = process.env.URL;
-var credentials = process.env.CREDENTIALS;
+var credentials = ""
+
+if (process.env.CREDENTIALS) {
+  var credentials = process.env.CREDENTIALS + '@';
+}
+
+var wsEndpoint  = `wss://${credentials}${url}/playwright/chromium/playwright-1.12.2`
 
 (async () => {
-  var browser = await chromium.connect({ timeout: 0, wsEndpoint: 'wss://'+ credentials + '@' + url + '/playwright/chromium/playwright-1.12.2' });
+  var browser = await chromium.connect({ timeout: 0, wsEndpoint: wsEndpoint });
   const page = await browser.newPage();
   await page.goto('http://whatsmyuseragent.org/');
   await page.screenshot({ path: 'screenshot.png' });
